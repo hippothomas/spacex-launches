e65e1b1e413ed507ff1f7756f1bba35f81e7f2db
Hippolyte <hippolyte.thomas1@gmail.com>
2020-09-02 17:14:13 +0200

    Ajout de VuexPersistence

--- src/store/FavoritesStore.js
+++ src/store/FavoritesStore.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 export default new Vuex.Store({
   state: {
@@ -21,6 +26,5 @@ export default new Vuex.Store({
     launches: (state) => state.launches,
     launch: (state) => ((id) => state.launches.find((launch) => launch.id === id)),
   },
-  actions: {
-  },
+  plugins: [vuexLocal.plugin],
 });
