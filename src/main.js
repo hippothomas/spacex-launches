6eb208308e0c546e1a0678ef18736b205d1d5d2d
Hippolyte <hippolyte.thomas1@gmail.com>
2020-09-02 14:54:08 +0200

    Ajout de VueX

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'es6-promise/auto'
 
+Vue.use(Vuex);
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
